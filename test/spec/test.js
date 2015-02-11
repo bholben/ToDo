a0eee3b0e798395d6477cadb0e2808903e43ba6f
Bob Holben <bholben@gmail.com>
2015-02-11 09:41:09 -0500

    Snag CSS from ToDoMVC, start down test path

--- test/spec/test.js
+++ test/spec/test.js
@@ -1,13 +1,53 @@
 console.log('Hi from test.js');
-/* global describe, it */
+
+// I should be able to add a todo item
+// I should be able to mark the todo item complete
+// I should be able to toggle the todo item back to uncomplete
+// I should be able to delete the todo item so it's not in view any more.
+//   > Hint: Just set a property deleted or something,
+//           don't actually remove it from the Array
+// Make sure your data matches your DOM at all times
 
 (function () {
   'use strict';
 
-  describe('Give it some context', function () {
-    describe('maybe a bit more context here', function () {
-      it('should run here few assertions', function () {
+  describe('About the ToDo List', function () {
 
+    beforeEach(function () {
+      window.task = new Task();
+      window.task.complete = false;
+      window.task.hidden = false;
+    });
+
+    describe('Able to add a task', function () {
+      it('should add a task', function () {
+        expect(task instanceof Task).to.equal(true);
+      });
+
+      it('should have a name', function () {
+        expect(task.name).to.not.be.undefined;
+      });
+    });
+
+    describe('Able to mark the task complete', function () {
+      it('should mark task complete', function () {
+        expect(task.complete).to.equal(false);
+        task.complete = true;
+        expect(task.complete).to.equal(true);
+      });
+    });
+
+    describe('Able to toggle the task back to incomplete', function () {
+      it('should change the task to incomplete', function () {
+        task.complete = false;
+        expect(task.complete).to.equal(false);
+      });
+    });
+
+    describe('Able to hide a task', function () {
+      it('should hide a task', function () {
+        task.hidden = false;
+        expect(task.hidden).to.equal(false);
       });
     });
   });
