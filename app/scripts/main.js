aa47d70ec0f3d90d805c29b9d859aa2a999781f1
Bob Holben <bholben@gmail.com>
2015-02-11 21:00:08 -0500

    Get toggle working

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,40 +1,29 @@
-console.log('Yo');
 
 // (function () {
 //   'use strict';
 
-  var tasks = [];
+  var taskTemplate = Handlebars.templates.task,
+      // footerTemplate = Handlebars.templates.footer,
+      $todoApp = $('#todoapp'),
+      $header = this.$todoApp.find('#header'),
+      $main = this.$todoApp.find('#main'),
+      // $footer = this.$todoApp.find('#footer'),
+      $form = this.$header.find('#form'),
+      $newTodo = this.$header.find('#new-todo'),
+      // $toggleAll = this.$main.find('#toggle-all'),
+      $todoList = this.$main.find('#todo-list'),
+      // $count = this.$footer.find('#todo-count'),
+      // $clearBtn = this.$footer.find('#clear-completed'),
+
+      tasks = [];
 
   // $.getJSON(todoURL).then(function (data) {
   //   tasks = tasks.concat(data);
   //   console.log('data has been fetched');
   // });
 
-  var taskTemplate = Handlebars.templates.task;
-  // var taskTemplate = _.template($('#taskTemplate').html());
 
   // var todoURL = 'http://tiy-atl-fe-server.heokuapp.com/collections/timstodos';
-
-  // var idGenerator = function () {
-  //   return _.random(0, 9999);
-  // };
-
-
-  var uuid = function () {
-    var i, random;
-    var uuid = '';
-
-    for (i = 0; i < 32; i++) {
-      random = Math.random() * 16 | 0;
-      if (i === 8 || i === 12 || i === 16 || i === 20) {
-        uuid += '-';
-      }
-      uuid += (i === 12 ? 4 : (i === 16 ? (random & 3 | 8) : random)).toString(16);
-    }
-
-    return uuid;
-  };
-
 
   var Task = function () {
     this.id = uuid();
@@ -45,19 +34,19 @@ console.log('Yo');
   Task.prototype = {
 
     create: function (title) {
-      this.title = title || '';
+      this.title = title;
       tasks.push(this);
       console.log(this);
-      // console.log(taskTemplate);
-      $('#todo-list').prepend(taskTemplate(this));
+      $todoList.prepend(taskTemplate(this));
+      $newTodo[0].value='';
 
       // $.post(todoURL, task);
 
     },
 
-    // toggleComplete: function () {
-    //   this.completed = !this.completed;
-    // },
+    toggleComplete: function () {
+      this.completed = !this.completed;
+    },
 
     // delete: function () {
 
@@ -69,33 +58,24 @@ console.log('Yo');
   };
 
 
-  var taskName, task;
+  // Event watchers
 
-  // Submit event watcher
-  $('form').on('submit', function (e) {
+  $form.on('submit', function (e) {
     e.preventDefault();
-
-    taskName = $(this).find('#new-todo').val();
-    // console.log(taskName);
-
-    task = new Task();
-    task.create(taskName);
-
+    var task = new Task();
+    task.create($newTodo.val());
   });
 
-  // $('#todo-list').on('click', 'li', function (e) {
-  //   var thisTask = event.target;
-  //   var thisTaskID = Number($(thisTask).attr('id'));
-  //       thisTaskID = Number(thisTask.id);  // same in vanilla JS
+  $todoList.on('click', 'input.toggle', function (e) {
+    var $task = $(e.target).closest('li'),
+        task = _.findWhere(tasks, {id: $task.attr('data-id')});
+    task.toggleComplete();
+    $task.toggleClass('completed');
+  });
 
-  //   var thisTaskInstance = _.findWhere(tasks, { id: thisTaskID });
-
-
-  //   thisTaskInstance.toggleComplete();
-
-  //   // $(thisTask).removeClass().addClass(thisTaskInstance.completed);
-
-  // });
+  $todoList.on('click', 'button.destroy', function () {
+    console.log('DESTROY');
+  });
 
 
 // });
